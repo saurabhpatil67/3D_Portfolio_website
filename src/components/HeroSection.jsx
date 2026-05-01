22399914d27fcdc8b6f4ed42b5d9b2154a852e3f
saurabhpatil67 <saurab.nik2002@gmail.com>
2026-05-02 01:38:50 +0530

    project completed successfully

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,10 +3,10 @@ import Spline from '@splinetool/react-spline';
 
 const HeroSection = () => {
     return (
-        <section className="h-screen bg-linear-to-b from-violet-900 to-black flex xl:flex-row flex-col-reverse items-center justify-between lg:px-24 px-10 relative overflow-hidden">
+        <section className="min-h-screen bg-linear-to-b from-violet-900 to-black flex xl:flex-row flex-col-reverse items-center justify-between lg:px-24 px-10 relative">
 
             {/* left section  */}
-            <div className="z-40 xl:mb-0 mb-[3%] ">
+            <div className="z-40 xl:mb-0 -mb-[20%] ">    
                 <motion.h1
                     initial={{ opacity: 0, y: 80 }}
                     animate={{ opacity: 1, y: 0 }}
@@ -38,10 +38,10 @@ const HeroSection = () => {
 
             {/* Right Section  */}
             <Spline 
-            className='absolute xl:right-[-30%] right-0 top-[-30%] lg:top-0 ' 
+            className='absolute xl:right-[-30%] right-0 top-[-20%] lg:top-0 ' 
             scene="https://prod.spline.design/ZJRaamaGcLM8x-gJ/scene.splinecode" />
 
-
+            
 
 
         </section>
